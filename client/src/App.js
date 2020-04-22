2c932219721b540214bad2c2d1dcd3e9b58c6afc
mike14747 <mike4747@oh.rr.com>
2020-04-21 23:37:52 -0400

    eslintrc is now working in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
+import Home from './pages/home/home';
 
 function App() {
     return (
         <Fragment>
             <h1>React Homepage</h1>
+            <Home />
         </Fragment>
     );
 }
