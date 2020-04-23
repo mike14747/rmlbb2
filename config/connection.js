1865be5bf352a15bef79a9c3d53529f87d5bf485
mike14747 <mike4747@oh.rr.com>
2020-04-22 23:08:52 -0400

    mongodb connection is working, but I need to figure out how to export the connection to the models

--- config/connection.js
+++ config/connection.js
@@ -12,19 +12,11 @@ async function connection() {
         const db = client.db(dbName);
 
         // console.log(db.databaseName);
-        const collections = await db.collections();
+        // const collections = await db.collections();
         // collections.forEach(collection => console.log(collection.collectionName));
 
-        const teams = await db.collection('teams').find({ name: 'Blaze' });
-        teams.forEach(team => console.log(team.name));
-
-        // const teams = await db.collection('teams').find();
-        // console.log(teams);
-
-        // const cursor = db.collection('teams').find();
-        // const result1 = await teams.find();
-        // const result2 = await result1.toArray();
-        // console.log(result2);
+        const result = await db.collection('teams').find().toArray();
+        console.log(result);
     } catch (error) {
         console.log(error);
     } finally {
