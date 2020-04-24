1d4c7e61785956e446ab55f1c5f9a118ec12b463
mike14747 <mike4747@oh.rr.com>
2020-04-23 21:07:17 -0400

    started the process of making a module mongodb connection pool... so far so good adding it to server.js through config/conn.js

--- server.js
+++ server.js
@@ -10,10 +10,45 @@ app.get('/', (req, res) => {
 
 app.use('/api', require('./controllers'));
 
-app.get('*', (req, res) => {
-    res.send('Page not found');
-});
+// app.get('*', (req, res) => {
+//     res.send('Page not found');
+// });
 
-app.listen(PORT, () => {
-    console.log('Server is listening on port ' + PORT);
+// app.listen(PORT, () => {
+//     console.log('Server is listening on port ' + PORT);
+// });
+
+const mongoInit = require('./config/conn');
+// const routes = require('./routes');
+const route = function (app, db) {
+    app.get('/teams', (req, res) => {
+        db.collection('teams').find({}).toArray((err, docs) => {
+            if (err) {
+                console.log(err);
+                res.error(err);
+            } else {
+                res.json(docs);
+            }
+        });
+    });
+    app.get('/blaze', (req, res) => {
+        db.collection('teams').find({ name: 'Blaze' }).toArray((err, doc) => {
+            if (err) {
+                console.log(err);
+                res.error(err);
+            } else {
+                res.json(doc);
+            }
+        });
+    });
+    return app;
+};
+
+mongoInit().then(db => {
+    // initialize the application once database connection is ready
+    route(app, db).listen(PORT, () => console.log('Listening on port ' + PORT));
+}).catch(err => {
+    console.error('Failed to make database connection!');
+    console.error(err);
+    process.exit(1);
 });
