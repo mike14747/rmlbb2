fc7d0f62ed175ff611e3ae86dc9cdd9dd9272416
mike14747 <mike4747@oh.rr.com>
2020-04-26 12:14:33 -0400

    cleaned up some unused code in server.js plus corrected the catch error handler in the mongodbConnect to /api

--- controllers/errorController.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-router.get('*', (req, res) => {
-    res.status(500).send('There is no connection to MongoDB!');
-});
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,62 +1,3 @@
-/*
-require('dotenv').config();
-const express = require('express');
-const app = express();
-
-const PORT = process.env.PORT || 3001;
-
-app.get('/', (req, res) => {
-    res.send('Sending this from the homepage');
-});
-
-app.use('/api', require('./controllers'));
-
-// app.get('*', (req, res) => {
-//     res.send('Page not found');
-// });
-
-// app.listen(PORT, () => {
-//     console.log('Server is listening on port ' + PORT);
-// });
-
-const mongoInit = require('./config/conn');
-// const routes = require('./routes');
-const route = function (app, db) {
-    app.get('/teams', (req, res) => {
-        db.collection('teams').find({}).toArray((err, docs) => {
-            if (err) {
-                console.log(err);
-                res.error(err);
-            } else {
-                res.json(docs);
-            }
-        });
-    });
-    app.get('/blaze', (req, res) => {
-        db.collection('teams').find({ name: 'Blaze' }).toArray((err, doc) => {
-            if (err) {
-                console.log(err);
-                res.error(err);
-            } else {
-                res.json(doc);
-            }
-        });
-    });
-    return app;
-};
-
-mongoInit().then(db => {
-    // initialize the application once database connection is ready
-    route(app, db).listen(PORT, () => console.log('Listening on port ' + PORT));
-}).catch(err => {
-    console.error('Failed to make database connection!');
-    console.error(err);
-    process.exit(1);
-});
-*/
-
-// test server.js
-
 require('dotenv').config();
 const express = require('express');
 const app = express();
@@ -72,7 +13,7 @@ mongodbConnect.serverConnect()
     })
     .catch((error) => {
         console.error('Failed to connect to the database!\n' + error);
-        app.get('*', (req, res) => {
+        app.get('/api', (req, res) => {
             res.status(500).send('There is no connection to MongoDB!');
         });
     })
