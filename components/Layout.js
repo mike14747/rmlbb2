ed7a3b81c729296beae96065fc9f013e6bb1a37f
mike14747 <mike4747@oh.rr.com>
2021-05-05 01:20:01 -0400

    Added a background image to the header with linear gradients on either side with media queries to tweak it. Added a sidebar in Layout.js.

--- components/Layout.js
+++ components/Layout.js
@@ -2,13 +2,22 @@ import PropTypes from 'prop-types';
 
 import Header from './Header';
 import Footer from './Footer';
+import EventsSidebar from './EventsSidebar';
 
 const Layout = ({ children }) => {
     return (
         <>
             <Header />
 
-            <main className="main-container">{children}</main>
+            <div className="wrapper">
+                <main className="main-container">{children}</main>
+                <section className="sidebar">
+                    <aside>
+                        <EventsSidebar />
+                    </aside>
+                    <aside>This will be the lower sidebar.</aside>
+                </section>
+            </div>
 
             <Footer />
         </>
