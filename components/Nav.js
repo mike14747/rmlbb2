7f2c6bdc6fe36244ab42cdb2088a8c20bd32f4e8
mike14747 <mike4747@oh.rr.com>
2021-05-13 19:28:14 -0400

    Restyled the nav. I like how it came out when it's in a row at 768+px screen sizes.

--- components/Nav.js
+++ components/Nav.js
@@ -6,35 +6,27 @@ const Nav = () => {
     return (
         <nav className={styles.nav}>
             <div className={styles.navdropdownContent}>
-                <div className={styles.item}>
-                    <Link href="/current"><a>Current Season +</a></Link>
-                </div>
-                <div className={styles.item}>
-                    <Link href="/downloads"><a>Downloads +</a></Link>
-                </div>
-                <div className={styles.item}>
-                    <Link href="/constitution"><a>Constitution</a></Link>
-                </div>
-                <div className={styles.item}>
-                    <Link href="/directory"><a>Manager Directory</a></Link>
-                </div>
-                <div className={styles.item}>
-                    <Link href="/lzp"><a>LZP Archive +</a></Link>
-                </div>
-                <div className={styles.item}>
-                    <Link href="/events"><a>Upcoming Events</a></Link>
-                </div>
-                <div className={styles.item}>
-                    <Link href="/board"><a>Message Board</a></Link>
-                </div>
-                <div className={styles.item}>
-                    <Link href="/champions"><a>Champions</a></Link>
-                </div>
-                <div className={styles.item}>
-                    <Link href="/contact"><a>Contact</a></Link>
-                </div>
-            </div>
-        </nav>
+
+                <Link href="/current"><a>Current Season +</a></Link>
+
+                <Link href="/downloads"><a>Downloads +</a></Link>
+
+                <Link href="/constitution"><a>Constitution</a></Link>
+
+                <Link href="/directory"><a>Manager Directory</a></Link>
+
+                <Link href="/lzp"><a>LZP Archive +</a></Link>
+
+                <Link href="/events"><a>Upcoming Events</a></Link >
+
+                <Link href="/board"><a>Message Board</a></Link >
+
+                <Link href="/champions"><a>Champions</a></Link >
+
+                <Link href="/contact"><a>Contact</a></Link>
+
+            </div >
+        </nav >
     );
 };
 
