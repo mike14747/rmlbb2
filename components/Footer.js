b334b578bbeeec2a5b6205aba77d27abd55b9bba
mike14747 <mike4747@oh.rr.com>
2021-05-31 16:34:09 -0400

    Added the identity-obj-proxy devDependency. Wrote a test for the footer using this new package.

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import styles from '../styles/Footer.module.css';
 
 const Footer = () => {
     return (
-        <footer className={'container ' + styles.footer}>
+        <footer data-testid="footer" className={'container ' + styles.footer}>
             &copy; 2015 RML Baseball
         </footer>
     );
---
+++ tests/Footer.test.js
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import Footer from '../components/Footer';
+
+describe('Test the Footer component', () => {
+    test('Footer renders the copyright logo and site name', () => {
+        const component = render(<Footer />);
+        const footerElement = component.getByTestId('footer');
+
+        expect(footerElement.textContent).toBe('© 2015 RML Baseball');
+    });
+});
