ae6c3819de7c5f4ebd64663e8d155bc0d69a8762
mike14747 <mike4747@oh.rr.com>
2021-05-31 23:49:54 -0400

    Added a news api function and wrote a test for it.

--- tests/newsAPI.test.js
+++ tests/newsAPI.test.js
@@ -1,9 +1,15 @@
 import { getSomeNewsItems, getAllNewsItems } from '../lib//api/news';
 
 describe('Make sure all the news items data functions are working', () => {
+    test('Checks to see if the most recent 10 news items are being returned', async () => {
+        const news = await getSomeNewsItems();
+
+        expect(news.length === 10).toBeTruthy();
+    });
+
     test('Checks to see if all news items are being returned', async () => {
         const news = await getAllNewsItems();
 
-        expect(news.length > 1).toBeTruthy();
+        expect(news.length > 10).toBeTruthy();
     });
 });
