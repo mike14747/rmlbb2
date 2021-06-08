c6f60f02327a32c50baae27065038f3e6e90e1d4
mike14747 <mike4747@oh.rr.com>
2021-06-08 18:35:54 -0400

    Added /components/BoardSidebar.js and its css module. Redid Nav.test.js to have a single long test.

---
+++ components/BoardSidebar.js
@@ -0,0 +1,11 @@
+import styles from '../styles/BoardSidebar.module.css';
+
+const BoardSidebar = () => {
+    return (
+        <>
+            This will be the recent message board posts sidebar.
+        </>
+    );
+};
+
+export default BoardSidebar;
--- components/Layout.js
+++ components/Layout.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Header from './Header';
 import Footer from './Footer';
 import EventsSidebar from './EventsSidebar';
+import BoardSidebar from '../components/BoardSidebar';
 
 const Layout = ({ children }) => {
     return (
@@ -16,7 +17,7 @@ const Layout = ({ children }) => {
                         <EventsSidebar />
                     </aside>
                     <aside>
-                        This will be the lower sidebar.
+                        <BoardSidebar />
                     </aside>
                 </section>
             </div>
