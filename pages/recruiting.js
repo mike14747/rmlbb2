8d6296519c4a80903c9233111543059886105493
mike14747 <mike4747@oh.rr.com>
2021-09-11 12:20:23 -0400

    The recruiting page is now displaying the text content from sanity via getStaticProps and a serverless function from /lib/api.

--- pages/recruiting.js
+++ pages/recruiting.js
@@ -1,9 +1,13 @@
+import PropTypes from 'prop-types';
 import Head from 'next/head';
-// import Link from 'next/link';
+import BlockContent from '@sanity/block-content-to-react';
+import noContainer from '../lib/noContainer';
+import { getNewManagerContent } from '../lib/api/miscPortableText';
 
 import styles from '../styles/recruiting.module.css';
 
-const Recruiting = () => {
+const Recruiting = ({ content }) => {
+    console.log(content);
     return (
         <>
             <Head>
@@ -17,16 +21,30 @@ const Recruiting = () => {
                     New Managers
                 </h2>
 
-                <p>
-                    The RML has an opening for a new manager.
-                </p>
+                <BlockContent
+                    blocks={content.content}
+                    serializers={noContainer}
+                />
 
                 <p>
-                    There will be info about the league here and a link to contact me for more info.
+                    <a href="mailto:mike4747@oh.rr.com">Contact us</a> for more info.
                 </p>
             </article>
         </>
     );
 };
+
+Recruiting.propTypes = {
+    content: PropTypes.object,
+};
+
+export async function getStaticProps() {
+    const content = await getNewManagerContent();
+
+    return {
+        props: { content },
+        revalidate: 600, // page regeneration can occur in 10 minutes
+    };
+}
 
 export default Recruiting;
