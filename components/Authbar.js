48af2d196b243a2437924163528041c700c9adbe
mike14747 <mike4747@oh.rr.com>
2021-09-17 23:08:12 -0400

    REmoved an uneeded useEffect import in the Authbar.

--- components/Authbar.js
+++ components/Authbar.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Link from 'next/link';
 import { signOut, useSession } from 'next-auth/react';
 import SignInInline from './SignInInline';
