3b0a1d3540c7827fda6ff2b7ecc784d161ff22e6
mike14747 <mike4747@oh.rr.com>
2021-11-26 13:34:12 -0500

    Added a privacy policy page and css module.

---
+++ pages/privacy.js
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import BlockContent from '@sanity/block-content-to-react';
+import { getPrivacyPolicyText } from '../lib/api/miscPortableText';
+import serializers from '../lib/serializers';
+
+import styles from '../styles/privacy.module.css';
+
+export default function Privacy({ content }){
+    return (
+        <>
+            <Head>
+                <title>
+                    RML Baseball - Privacy Policy
+                </title>
+            </Head>
+
+            <article className={styles.privacyContainer}>
+                <h2 className="page-heading">
+                    Privacy Policy
+                </h2>
+
+                {!content?.content
+                    ? <p className="error">An error occurred fetching data.</p>
+                    : <BlockContent
+                        blocks={content.content}
+                        serializers={serializers}
+                    />
+                }
+            </article>
+        </>
+    );
+}
+
+Privacy.propTypes = {
+    content: PropTypes.object,
+};
+
+export async function getStaticProps() {
+    const content = await getPrivacyPolicyText();
+
+    return {
+        props: { content },
+        revalidate: 600, // page regeneration can occur in 10 minutes
+    };
+}
