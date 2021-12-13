e245f038401383115731fd56d30d22925756b7c8
mike14747 <mike4747@oh.rr.com>
2021-12-13 00:52:27 -0500

    Updated the Button component.

--- components/Button.js
+++ components/Button.js
@@ -4,15 +4,21 @@ import styles from '../styles/Button.module.css';
 
 const sizes = ['small', 'medium', 'large', 'specialSize'];
 const variants = ['outlined', 'text', 'contained', 'special'];
-const types = ['submit'];
+const types = ['button', 'submit', 'reset'];
 
 export default function Button({ children, size, variant, onClick, type }) {
     const btnSize = sizes.includes(size) ? size : 'medium';
     const btnVariant = variants.includes(variant) ? variant : 'contained';
-    const btnType = types.includes(type) ? type : null;
+    const btnType = types.includes(type) ? type : 'button';
 
     return (
-        <button {...(onClick && { onClick })} {...(type && { type: btnType })} className={`${styles.btn} ${styles[`${btnSize}`]} ${styles[`${btnVariant}`]}`}>{children}</button>
+        <button
+            onClick={onClick }
+            type={btnType}
+            className={`${styles.btn} ${styles[`${btnSize}`]} ${styles[`${btnVariant}`]}`}
+        >
+            {children}
+        </button>
     );
 }
 
