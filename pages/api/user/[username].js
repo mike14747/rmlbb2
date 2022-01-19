79550a76a6bd4ae621127dc2c8f7b72a1fe0e707
mike14747 <mike4747@oh.rr.com>
2022-01-18 21:45:16 -0500

    Updates to the user profile and password reset api routes and serverless functions. Moved the email address from the nodemailer config to an environment variable. Still working on the password reset link.

--- pages/api/user/[username].js
+++ pages/api/user/[username].js
@@ -1,28 +1,17 @@
 import { getSession } from 'next-auth/react';
-import { getUserProfile, updateUserProfile } from '../../../lib/api/user';
+import { getUserProfile } from '../../../lib/api/user';
 
 export default async function user(req, res) {
     const session = await getSession({ req });
     if (!session || session.user.name !== req.query.username) res.status(401).end();
 
-    switch (req.method) {
-        case 'GET':
-            try {
-                const response = await getUserProfile(req.query.username);
-                response ? res.status(200).json(response) : res.status(500).end();
-            } catch (error) {
-                res.status(500).end();
-            }
-            break;
-        case 'PUT':
-            try {
-                const response = await updateUserProfile(req.query.username);
-                response ? res.status(200).json(response) : res.status(500).end();
-            } catch (error) {
-                res.status(500).end();
-            }
-            break;
-        default:
-            res.status(401).end();
+    if (req.method === 'GET') {
+        try {
+            const response = await getUserProfile(req.query.username);
+            if (!response) res.status(500).end();
+            response?.length === 1 ? res.status(200).json(response) : res.status(400).end();
+        } catch (error) {
+            res.status(500).end();
+        }
     }
 }
