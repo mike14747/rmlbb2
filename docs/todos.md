6b727b063ca7b56b67a8c763de1759e6fa9db541
mike14747 <mike4747@oh.rr.com>
2022-02-02 21:18:43 -0500

    Converted the directory page to the auth system. Updated /docs/todos.md. Changed the background color on the ManagerCard cards.

--- docs/todos.md
+++ docs/todos.md
@@ -1,4 +1,4 @@
-Get the forgot username/password links working. (done, but required a lot of testing)
+### Need to do
 
 Figure out a new message board solution that can have our old board imported into it. (in progress)
 
@@ -10,12 +10,20 @@ Style the "To main content" link.
 
 Figure out how to get the homepage test working in jest.
 
-~~Should the SignInMini include links for users who have forgotten their login info. (Done in the SignIn component)~~ I removed the SignInMini component from use because I reworked the way authenticated pages work.
-
 Added news items to sanity.io.
 
 Add file downloads to sanity.io.
 
-Change over the directory and profile pages to the new authentication system.
+Update managers in sanity.io.
+
+---
+
+### Done
+
+Get the forgot username/password links working. (done, but required a lot of testing)
+
+Should the SignInMini include links for users who have forgotten their login info. (Done in the SignIn component) I removed the SignInMini component from use because I reworked the way authenticated pages work.
 
 Make the TopInfo, Authbar and Footer components not be full width.
+
+Change over the directory and profile pages to the new authentication system.
--- pages/directory.js
+++ pages/directory.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
 import Head from 'next/head';
 import Loading from '../components/Loading';
-import SignIn from '../components/SignIn';
 import ManagerCard from '../components/ManagerCard';
 
 import styles from '../styles/directory.module.css';
@@ -10,6 +10,8 @@ import styles from '../styles/directory.module.css';
 const Directory = () => {
     const { data: session, status } = useSession();
     const loading = status === 'loading';
+
+    const router = useRouter();
 
     const [managers, setManagers] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
@@ -26,7 +28,6 @@ const Directory = () => {
                 .then(data => {
                     setManagers(data);
                     setError(null);
-                    setIsLoading(false);
                 })
                 .catch(error => {
                     if (error.name === 'AbortError') {
@@ -35,9 +36,9 @@ const Directory = () => {
                         console.log(error);
                         setManagers(null);
                         setError('An error occurred fetching data.');
-                        setIsLoading(false);
                     }
-                });
+                })
+                .finally(() => setIsLoading(false));
         } else {
             setManagers(null);
         }
@@ -46,6 +47,10 @@ const Directory = () => {
     }, [session]);
 
     if (typeof window !== 'undefined' && loading) return null;
+
+    if (!session) {
+        router.push('/login?url=/directory');
+    }
 
     return (
         <>
@@ -60,38 +65,30 @@ const Directory = () => {
                     Directory
                 </h2>
 
-                {loading && <Loading />}
+                {error && <p className="error">{error}</p>}
 
-                {!session && <SignIn />}
+                {isLoading && <Loading />}
 
-                {session &&
-                    <>
-                        {error && <p className="error">{error}</p>}
-
-                        {isLoading && <Loading />}
-
-                        {managers?.length > 0 &&
-                            <div className={styles.directoryContainer}>
-                                {managers.map(conf => (
-                                    <div key={conf.conference} className={styles.conferenceContainer}>
-                                        <h3 className={styles.conferenceHeading}>
-                                            {conf.conference} Conference
-                                        </h3>
-                                        {conf.divisions.map(div => (
-                                            <div key={div.division} className={styles.divisionContainer}>
-                                                <h4 className={styles.divisionHeading}>
-                                                    <span className={styles.divisionPrefix}>Division: </span> {div.division}
-                                                </h4>
-                                                {div.teams.map(team => (
-                                                    <ManagerCard key={team.team} manager={team} />
-                                                ))}
-                                            </div>
+                {managers?.length > 0 &&
+                    <div className={styles.directoryContainer}>
+                        {managers.map(conf => (
+                            <div key={conf.conference} className={styles.conferenceContainer}>
+                                <h3 className={styles.conferenceHeading}>
+                                    {conf.conference} Conference
+                                </h3>
+                                {conf.divisions.map(div => (
+                                    <div key={div.division} className={styles.divisionContainer}>
+                                        <h4 className={styles.divisionHeading}>
+                                            <span className={styles.divisionPrefix}>Division: </span> {div.division}
+                                        </h4>
+                                        {div.teams.map(team => (
+                                            <ManagerCard key={team.team} manager={team} />
                                         ))}
                                     </div>
                                 ))}
                             </div>
-                        }
-                    </>
+                        ))}
+                    </div>
                 }
             </article>
         </>
