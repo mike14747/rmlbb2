70fd1bf95b78cb5626e3c7c0fb1ca5a21a77e0fb
mike14747 <mike4747@oh.rr.com>
2022-03-04 19:31:31 -0500

    Added 4 admin pages without any functionality.

---
+++ pages/admin/add-user.js
@@ -0,0 +1,34 @@
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
+import Head from 'next/head';
+
+// import styles from '../styles/admin.module.css';
+
+export default function AddUser() {
+    const { data: session, status } = useSession();
+    const loading = status === 'loading';
+
+    const router = useRouter();
+
+    if (typeof window !== 'undefined' && loading) return null;
+
+    if (!session || !session.user || !session.user.role || session.user.role !== 'admin') {
+        router.push('/404');
+    }
+
+    return (
+        <>
+            <Head>
+                <title>
+                    RML Baseball - Admin
+                </title>
+            </Head>
+
+            <article>
+                <h2 className="page-heading">
+                    Add User
+                </h2>
+            </article>
+        </>
+    );
+}
