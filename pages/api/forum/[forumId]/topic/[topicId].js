c7858c90cb10ae074635526d54222d2f30c4bc54
mike14747 <mike4747@oh.rr.com>
2022-03-15 22:26:04 -0400

    Broke up the serverless functions for getting a topic and its replies into 2 serverless functions.

--- pages/api/forum/[forumId]/topic/[topicId].js
+++ pages/api/forum/[forumId]/topic/[topicId].js
@@ -1,5 +1,5 @@
 import { getSession } from 'next-auth/react';
-import { getForumTopic } from '../../../../../lib/api/forum';
+import { getForumTopic, getTopicReplies } from '../../../../../lib/api/forum';
 
 export default async function forumTopic(req, res) {
     if (req.method === 'GET') {
@@ -8,8 +8,14 @@ export default async function forumTopic(req, res) {
         if (!req.query.forumId || !req.query.topicId) res.status(400).end();
 
         try {
-            const response = await getForumTopic(parseInt(req.query.forumId), parseInt(req.query.topicId));
-            response ? res.status(200).json(response) : res.status(400).end();
+            const topicData = await getForumTopic(parseInt(req.query.forumId), parseInt(req.query.topicId));
+
+            let repliesData = [];
+            if (topicData?.replies?.length > 0) {
+                repliesData = await getTopicReplies(topicData.replies);
+            }
+
+            topicData ? res.status(200).json({ topicData, repliesData }) : res.status(400).end();
         } catch (error) {
             console.error(error);
             res.status(500).end();
