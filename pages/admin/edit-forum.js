bbdf573af27ef1e55b48fcf22fe81079c1845926
mike14747 <mike4747@oh.rr.com>
2022-03-28 22:53:41 -0400

    Lots of work on the edit-forum page, api route and serverless function, but it isn't functional yet.

--- pages/admin/edit-forum.js
+++ pages/admin/edit-forum.js
@@ -1,7 +1,75 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
+import Head from 'next/head';
+import FormInputForForumName from '../../components/FormInputForForumName';
+import FormInputForActive from '../../components/FormInputForActive';
+import Button from '../../components/Button';
+import Loading from '../../components/Loading';
+
+import styles from '../../styles/admin.module.css';
+
 export default function EditForum() {
-    return (
-        <>
-            Edit forum page
-        </>
-    );
+    const { data: session, status } = useSession();
+    const loading = status === 'loading';
+
+    const router = useRouter();
+
+    const [forums, setForums] = useState(null);
+    const [forumError, setForumError] = useState(null);
+    const [forumUpdateMsg, setForumUpdateMsg] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleNewForumNameSubmit = async (e) => {
+        e.preventDefault();
+
+        setIsLoading(true);
+
+        const res = await fetch('/api/forum/add-forum', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+            },
+            body: JSON.stringify({}),
+        });
+
+        if (res.status !== 201) {
+            res.status === 400 && forumError('An error occurred. Forum name did not make it to the server.');
+            res.status === 401 && forumError('An error occurred. You do not have permission for this operation.');
+            res.status === 409 && forumError('An error occurred. The forum name you submitted is already in use.');
+            res.status === 500 && forumError('A server error occurred. Please try your update again.');
+            setForumUpdateMsg('');
+        }
+
+        if (res.status === 201) {
+            // setForumName('');
+            // forumError(null);
+            // setForumUpdateMsg('The new forum: "' + forumName + '" has been successfully added!');
+        }
+    };
+
+    if (typeof window !== 'undefined' && loading) return null;
+
+    if (!session?.user?.role || session.user.role !== 'admin') router.push('/');
+
+    if (session && session?.user?.role === 'admin') {
+
+        return (
+            <>
+                <Head>
+                    <title>
+                        RML Baseball - Admin
+                    </title>
+                </Head>
+
+                <article className={styles.adminContainer}>
+                    <h2 className={'page-heading ' + styles.adminPageHeading}>
+                        Edit Forum
+                    </h2>
+
+                    {isLoading && <Loading />}
+                </article>
+            </>
+        );
+    }
 }
--- pages/api/forum/edit-forum-name.js
+++ pages/api/forum/edit-forum-name.js
@@ -1,14 +1,14 @@
 import { getSession } from 'next-auth/react';
-import { editForumActiveStatus } from '../../../lib/api/forum';
+import { editForumName } from '../../../lib/api/forum';
 
-export default async function forumActiveStatus(req, res) {
+export default async function newForumName(req, res) {
     const session = await getSession({ req });
     if (!session?.user?.role || session.user.role !== 'admin') res.status(401).end();
-    if (!req?.body?.id, !req?.body?.active) return res.status(400).end();
+    if (!req?.body?.id || !req?.body?.name) return res.status(400).end();
 
     if (req.method === 'POST') {
         try {
-            const response = await editForumActiveStatus(req.body.id, req.body.active);
+            const response = await editForumName(req.body.id, req.body.name);
             response?.code ? res.status(response.code).end() : res.status(500).end();
         } catch (error) {
             console.error(error);
