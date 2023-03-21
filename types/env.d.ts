6056bd62b8a3800ba40fdd09cf769015553a57b2
mike14747 <mike4747@oh.rr.com>
2023-03-21 12:43:31 -0400

    Added /types/env.d.ts.

---
+++ types/env.d.ts
@@ -0,0 +1,29 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            NODE_ENV: 'development' | 'production';
+            MONGODB_URI: string;
+            MONGODB_DB: string;
+            SANITY_PUBLIC_QUERY_URL: string;
+            SANITY_PUBLIC_MUTATION_URL: string;
+            SANITY_PUBLIC_API_TOKEN: string;
+            SANITY_PRIVATE_QUERY_URL: string;
+            SANITY_API_TOKEN: string;
+            SANITY_PRIVATE_MUTATION_URL: string;
+            SANITY_PRIVATE_API_TOKEN: string;
+            NEXT_PUBLIC_BASE_FILE_QUERY_URL: string;
+            NEXT_PUBLIC_BASE_FILE_DOWNLOAD_URL: string;
+            NEXTAUTH_URL: string;
+            NEXTAUTH_SECRET: string;
+            NO_REPLY_EMAIL: string;
+            GOOGLE_CLIENT_ID: string;
+            GOOGLE_CLIENT_SECRET: string;
+            GOOGLE_REFRESH_TOKEN: string;
+            BASE_URL: string;
+            INITIAL_NEWS_ITEMS: number;
+            NEWS_ITEMS_INCREMENT: number;
+        }
+    }
+}
+
+export { };
