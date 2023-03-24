3db67da635dd2096b535efefdd198c1321af9138
mike14747 <mike4747@oh.rr.com>
2023-03-24 15:32:27 -0400

    Added a type for params in the test api route.

--- app/api/test/[id]/route.ts
+++ app/api/test/[id]/route.ts
@@ -1,6 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(request: NextRequest, { params }) {
+type IdParams = {
+    params: {
+        id: string;
+    }
+}
+
+export async function GET(request: NextRequest, { params }: IdParams) {
     const id = params.id;
     const { searchParams } = request.nextUrl;
     const status = searchParams.get('status');
