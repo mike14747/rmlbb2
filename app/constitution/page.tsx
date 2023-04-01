2f7e8bae91cafaf7128c602b175b4f543b935be4
mike14747 <mike4747@oh.rr.com>
2023-03-31 22:16:28 -0400

    Moved the constitution page to the appDir with typescript.

---
+++ app/constitution/page.tsx
@@ -0,0 +1,38 @@
+import { PortableText } from '@portabletext/react';
+import { getConstitutionContent } from '../../lib/api/miscPortableText';
+import components from '../../lib/helpers/portalTextComponents';
+import type { Metadata } from 'next';
+import { Suspense } from 'react';
+import Loading from '../components/Loading';
+
+import styles from '../../styles/constitution.module.css';
+import blockContentStyles from '../../styles/blockContent.module.css';
+
+export const metadata: Metadata = {
+    title: 'RML Baseball - Constitution',
+};
+
+export default async function Constitution() {
+    const res = await getConstitutionContent();
+
+    return (
+        <article className={styles.constitutionContainer + ' mw-90ch ' + blockContentStyles.blockContentContainer}>
+            <h2 className="page-heading">
+                Constitution
+            </h2>
+
+            <Suspense fallback={<Loading />}>
+                {!res && <p className="error">An error occurred fetching data.</p>}
+
+                {res?.content && res.content.length < 1 && <p>No content was found. Please try again later.</p>}
+
+                {res?.content && res.content.length > 1 &&
+                    <PortableText
+                        value={res.content}
+                        components={components}
+                    />
+                }
+            </Suspense>
+        </article>
+    );
+}
