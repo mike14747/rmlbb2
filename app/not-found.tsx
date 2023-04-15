c1b2fbb62818ba1a6bb3eda4bfdc6a1ee02b542f
mike14747 <mike4747@oh.rr.com>
2023-04-14 23:52:26 -0400

    Upgraded the app to next.js v13.3. Populated the admin edit-user route file. Fixed the bugs I was experiencing with next.js v13.3. Added a global not-found.tsx page.

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'RML Baseball - Not Found',
+};
+
+export default function NotFound() {
+    return (
+        <main id="main">
+            <article className="mw-90ch error-container">
+                <h2>
+                    Error 404!
+                </h2>
+
+                <p className="error">An error has occurred.</p>
+                <p className="error">The page you are looking for does not exist!</p>
+            </article>
+        </main>
+    );
+}
